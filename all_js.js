c2498c85d55cf78af68fe8c9ea16b1c2f0f4bce8
Nicole Jackson <38741156+Blandianson@users.noreply.github.com>
2018-11-28 16:53:00 +1300

    Working and passing prism data directly from prism to all_js

--- all_js.js
+++ all_js.js
@@ -2,11 +2,10 @@
 //Date: 27/11/2018, 
 //Desc: Takes a csv of dates and vals as converts to a heatmap calendar representation.
 
-var prism_str = (document).getElementById("prism_data").innerHTML;
+//var prism_str = (document).getElementById("prism_data").innerHTML;
 
-//function calendarShow(data, element){
-
-    //var prism_str = $("#prism_data").data();
+function calendarShow(prism_str){
+    //var prism_str = $("#prism_data").data("pr_dat");
     var prism = JSON.parse(prism_str);
 
 
@@ -40,144 +39,149 @@ var prism_str = (document).getElementById("prism_data").innerHTML;
         else{ date_var += Math.round(str_var) + "\n"; } 
     });
     var csv = d3.csv.parse(date_var, function(item) { return item; });	//Parses the var to csv format
-//}
+
+    formCalendar(min, max, date_var, csv);
+}
 
 
 //Heatmap Calendar by Kathy Zhou source: http://bl.ocks.org/KathyZ/c2d4694c953419e0509b
 
-var width = 960,
-        height = 750,
-        cellSize = 25; // cell size
+function formCalendar(min, max, date_var, csv){
 
-var no_months_in_a_row = Math.floor(width / (cellSize * 7 + 50));
-var shift_up = cellSize * 3;
+    var width = 960,
+            height = 750,
+            cellSize = 25; // cell size
 
-var day = d3.time.format("%w"), // day of the week
-    day_of_month = d3.time.format("%e") // day of the month
-    day_of_year = d3.time.format("%j")
-    week = d3.time.format("%U"), // week number of the year
-    month = d3.time.format("%m"), // month number
-    year = d3.time.format("%Y"),
-    percent = d3.format(".1%"),
-    format = d3.time.format("%Y-%m-%d");
+    var no_months_in_a_row = Math.floor(width / (cellSize * 7 + 50));
+    var shift_up = cellSize * 3;
 
-var color = d3.scale.quantize()
-    .domain([-.05, .05])
-    .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
+    var day = d3.time.format("%w"), // day of the week
+        day_of_month = d3.time.format("%e") // day of the month
+        day_of_year = d3.time.format("%j")
+        week = d3.time.format("%U"), // week number of the year
+        month = d3.time.format("%m"), // month number
+        year = d3.time.format("%Y"),
+        percent = d3.format(".1%"),
+        format = d3.time.format("%Y-%m-%d");
 
-var svg = d3.select("#chart").selectAll("svg")
-    .data(d3.range(min, max+1))
-    .enter().append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("class", "RdYlGn")
-    .append("g")
+    var color = d3.scale.quantize()
+        .domain([-.05, .05])
+        .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
 
-var rect = svg.selectAll(".day")
-    .data( function(d) { 
-        return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1));
-    })
-    .enter().append("rect")
-    .attr("class", "day")
-    .attr("width", cellSize)
-    .attr("height", cellSize)
-    .attr("x", function(d) {
-        var month_padding = 1.2 * cellSize*7 * ((month(d)-1) % (no_months_in_a_row));
-        return day(d) * cellSize + month_padding; 
-    })
-    .attr("y", function(d) { 
-        var week_diff = week(d) - week(new Date(year(d), month(d)-1, 1) );
-        var row_level = Math.ceil(month(d) / (no_months_in_a_row));
-        return (week_diff*cellSize) + row_level*cellSize*8 - cellSize/2 - shift_up;
-    })
-    .datum(format);
+    var svg = d3.select("#chart").selectAll("svg")
+        .data(d3.range(min, max+1))
+        .enter().append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "RdYlGn")
+        .append("g")
 
-var month_titles = svg.selectAll(".month-title")  // Jan, Feb, Mar and the whatnot
-        .data(function(d) { 
-        return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-    .enter().append("text")
-        .text(monthTitle)
-        .attr("x", function(d, i) {
-        var month_padding = 1.2 * cellSize*7* ((month(d)-1) % (no_months_in_a_row));
-        return month_padding;
+    var rect = svg.selectAll(".day")
+        .data( function(d) { 
+            return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1));
         })
-        .attr("y", function(d, i) {
-        var week_diff = week(d) - week(new Date(year(d), month(d)-1, 1) );
-        var row_level = Math.ceil(month(d) / (no_months_in_a_row));
-        return (week_diff*cellSize) + row_level*cellSize*8 - cellSize - shift_up;
+        .enter().append("rect")
+        .attr("class", "day")
+        .attr("width", cellSize)
+        .attr("height", cellSize)
+        .attr("x", function(d) {
+            var month_padding = 1.2 * cellSize*7 * ((month(d)-1) % (no_months_in_a_row));
+            return day(d) * cellSize + month_padding; 
         })
-        .attr("class", "month-title")
-        .attr("d", monthTitle);
+        .attr("y", function(d) { 
+            var week_diff = week(d) - week(new Date(year(d), month(d)-1, 1) );
+            var row_level = Math.ceil(month(d) / (no_months_in_a_row));
+            return (week_diff*cellSize) + row_level*cellSize*8 - cellSize/2 - shift_up;
+        })
+        .datum(format);
 
-var year_titles = svg.selectAll(".year-title")  // Jan, Feb, Mar and the whatnot
-        .data(function(d) { 
-        return d3.time.years(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-    .enter().append("text")
-        .text(yearTitle)
-        .attr("x", function(d, i) { return width/2 - 100; })
-        .attr("y", function(d, i) { return cellSize*5.5 - shift_up; })
-        .attr("class", "year-title")
-        .attr("d", yearTitle);
+    var month_titles = svg.selectAll(".month-title")  // Jan, Feb, Mar and the whatnot
+            .data(function(d) { 
+            return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .enter().append("text")
+            .text(monthTitle)
+            .attr("x", function(d, i) {
+            var month_padding = 1.2 * cellSize*7* ((month(d)-1) % (no_months_in_a_row));
+            return month_padding;
+            })
+            .attr("y", function(d, i) {
+            var week_diff = week(d) - week(new Date(year(d), month(d)-1, 1) );
+            var row_level = Math.ceil(month(d) / (no_months_in_a_row));
+            return (week_diff*cellSize) + row_level*cellSize*8 - cellSize - shift_up;
+            })
+            .attr("class", "month-title")
+            .attr("d", monthTitle);
 
-
-//  Tooltip Object
-var tooltip = d3.select("body")
-    .append("div").attr("id", "tooltip")
-    .style("position", "absolute")
-    .style("z-index", "10")
-    .style("visibility", "hidden")
-    .text("a simple tooltip");
-
-var data = d3.nest()
-  .key(function(d) {
-    return d.Date;
-  })
-  .rollup(function(d) {
-    return (d[0].Var/100);                              //Divides the data to make them a percentage.
-  })
-  .map(csv);
-
-rect.filter(function(d) {
-    return d in data;
-  })
-  .attr("class", function(d) {
-    return "day " + color((data[d])/40);				//Divides the data so heatmap colours aren't too extreme.
-  })
-  .select("title")
-  .text(function(d) {
-    return d + ": " + percent(data[d]);
-  });
+    var year_titles = svg.selectAll(".year-title")  // Jan, Feb, Mar and the whatnot
+            .data(function(d) { 
+            return d3.time.years(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+        .enter().append("text")
+            .text(yearTitle)
+            .attr("x", function(d, i) { return width/2 - 100; })
+            .attr("y", function(d, i) { return cellSize*5.5 - shift_up; })
+            .attr("class", "year-title")
+            .attr("d", yearTitle);
 
 
-    //  Tooltip
-    rect.on("mouseover", mouseover);
-    rect.on("mouseout", mouseout);
-    function mouseover(d) {
-    tooltip.style("visibility", "visible");
-    var percent_data = (data[d] !== undefined && isNaN(data[d]) === false ) ? percent(data[d]) : "No Data";
-    var purchase_text = d + ": " + percent_data;
+    //  Tooltip Object
+    var tooltip = d3.select("body")
+        .append("div").attr("id", "tooltip")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .text("a simple tooltip");
 
-    tooltip.transition()        
-                .duration(200)      
-                .style("opacity", .9);      
-    tooltip.html(purchase_text)  
-                .style("left", (d3.event.pageX)+30 + "px")     
-                .style("top", (d3.event.pageY) + "px"); 
+    var data = d3.nest()
+    .key(function(d) {
+        return d.Date;
+    })
+    .rollup(function(d) {
+        return (d[0].Var/100);                              //Divides the data to make them a percentage.
+    })
+    .map(csv);
+
+    rect.filter(function(d) {
+        return d in data;
+    })
+    .attr("class", function(d) {
+        return "day " + color((data[d])/40);				//Divides the data so heatmap colours aren't too extreme.
+    })
+    .select("title")
+    .text(function(d) {
+        return d + ": " + percent(data[d]);
+    });
+
+
+        //  Tooltip
+        rect.on("mouseover", mouseover);
+        rect.on("mouseout", mouseout);
+        function mouseover(d) {
+        tooltip.style("visibility", "visible");
+        var percent_data = (data[d] !== undefined && isNaN(data[d]) === false ) ? percent(data[d]) : "No Data";
+        var purchase_text = d + ": " + percent_data;
+
+        tooltip.transition()        
+                    .duration(200)      
+                    .style("opacity", .9);      
+        tooltip.html(purchase_text)  
+                    .style("left", (d3.event.pageX)+30 + "px")     
+                    .style("top", (d3.event.pageY) + "px"); 
+        }
+        function mouseout (d) {
+        tooltip.transition()        
+                .duration(500)      
+                .style("opacity", 0); 
+        var $tooltip = $("#tooltip");
+        $tooltip.empty();
+        }
+
+    function dayTitle (t0) {
+        return t0.toString().split(" ")[2];
     }
-    function mouseout (d) {
-    tooltip.transition()        
-            .duration(500)      
-            .style("opacity", 0); 
-    var $tooltip = $("#tooltip");
-    $tooltip.empty();
+    function monthTitle (t0) {
+        return t0.toLocaleString("en-us", { month: "long" });
     }
-
-function dayTitle (t0) {
-    return t0.toString().split(" ")[2];
-}
-function monthTitle (t0) {
-    return t0.toLocaleString("en-us", { month: "long" });
-}
-function yearTitle (t0) {
-    return t0.toString().split(" ")[3];
+    function yearTitle (t0) {
+        return t0.toString().split(" ")[3];
+    }
 }
